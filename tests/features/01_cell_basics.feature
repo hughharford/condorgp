cc42affe6137cc76775bdd33ed804835dccacde1
hughharford <hugh.harford@poscoconsulting.com>
2022-05-23 06:26:24 +0100

    added remove cells test (bdd)

--- tests/features/01_cell_basics.feature
+++ tests/features/01_cell_basics.feature
@@ -6,7 +6,7 @@ Feature: Condor cells run as required
 
 
   # @add
-  Scenario Outline: Add a cell
+  Scenario Outline: Add cells
     Given the nest has "<initial>" cells
     When "<some>" cells are added to the nest
     Then the nest contains "<total>" cells
@@ -16,3 +16,13 @@ Feature: Condor cells run as required
       | 0       | 3    | 3     |
       | 3       | 4    | 7     |
       | 7       | 5    | 12    |
+
+  Scenario Outline: Remove cells
+    Given the existing nest has "<initial>" cells
+    When "<some>" cells are removed
+    Then the nest now contains "<total>" cells
+
+    Examples:
+      | initial | some | total |
+      | 12      | 8    | 4     |
+      | 4       | 4    | 0     |
--- tests/step_defs/test_01_cell_basics_steps.py
+++ tests/step_defs/test_01_cell_basics_steps.py
@@ -6,7 +6,6 @@ EXTRA_TYPES = {
     'Number': int,
 }
 
-
 CONVERTERS = {
     'initial': int,
     'some': int,
@@ -15,15 +14,14 @@ CONVERTERS = {
 
 scenarios('../features/01_cell_basics.feature')
 """
-    Scenario: Cell creation
-    Given pre-created cells
-    When another cell is created
-    Then the celllist is incremented
+  Scenario Outline: Add cells
+    Given the nest has "<initial>" cells
+    When "<some>" cells are added to the nest
+    Then the nest contains "<total>" cells
 """
 
-# the nest has "<initial>" cells
 @given(parsers.cfparse('the nest has "{initial:Number}" cells', extra_types=EXTRA_TYPES), target_fixture='cells')
-@given('the basket has "<initial>" cells', target_fixture='cells')
+@given('the next has "<initial>" cells', target_fixture='cells')
 def cells(initial):
     assert len(Cell.getcelllist()) == initial
 
@@ -37,6 +35,32 @@ def when_somecellsarecreated(some):
 @then('the nest contains "<total>" cells')
 def then_thecelllistisincremented(total):
     assert len(Cell.getcelllist()) == total
+
+
+"""
+  Scenario Outline: Remove cells
+    Given the existing nest has "<initial>" cells
+    When "<some>" cells are removed
+    Then the nest now contains "<total>" cells
+"""
+
+@given(parsers.cfparse('the existing nest has "{initial:Number}" cells', extra_types=EXTRA_TYPES), target_fixture='cells')
+@given('the existing nest has "<initial>" cells', target_fixture='cells')
+def cells(initial):
+    assert len(Cell.getcelllist()) == initial
+
+@when(parsers.cfparse('"{some:Number}" cells are removed', extra_types=EXTRA_TYPES))
+@when('"<some>"  cells are removed')
+def when_somecellsarecreated(some):
+    for i in range(some):
+        Cell(f'dummy_ref1 {i}','PROTOTYPE')
+
+@then(parsers.cfparse('the nest now contains "{total:Number}" cells', extra_types=EXTRA_TYPES))
+@then('the nest now contains "<total>" cells')
+def then_thecelllistisincremented(total):
+    assert len(Cell.getcelllist()) == total
+
+
 
 # """
 #     Scenario: Cell id
