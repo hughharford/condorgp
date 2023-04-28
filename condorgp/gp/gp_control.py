9deb1cdec88ae6e740ad7c990d362be9063f9547
hughharford <hugh.harford@poscoconsulting.com>
2023-04-28 14:42:13 +0100

    readjust evals in gpcontrol

--- condorgp/gp/gp_control.py
+++ condorgp/gp/gp_control.py
@@ -19,7 +19,7 @@ class GpControl:
         self.log.info(f"{'>'*10}, GpControl Initialising {'>'*10}")
         # default population set and evaluator (fitness function)
         self.default_pset = 'default_untyped'
-        self.default_eval = self.evalIntoAndFromLean
+        self.default_eval = self.eval_nautilus
         self.default_tidyup = 1
         self.run_backtest = 1 # 1 = run lean in evaluation func, 0 = don't
 
@@ -107,54 +107,6 @@ class GpControl:
     def get_logbook(self):
         return self.gp.logbook
 
-    def evalIntoAndFromLean(self, individual):
-        # Transform the tree expression in a callable function
-        func = self.gp.toolbox.compile(expr=individual)
-        # run Lean with algo and config:
-        input_ind = 'IndBasicAlgo1.py'
-        config_to_run = test_dict['CONDOR_TEST_CONFIG_FILE_1']
-        self.util.copy_config_in(input_ind)
-        self.util.copy_algo_in(input_ind)
-        self.lean.run_lean_via_CLI(input_ind, config_to_run)
-        # get fitness
-        Return_over_MDD = 'STATISTICS:: Return Over Maximum Drawdown'
-        got = self.util.get_key_line_in_lim(Return_over_MDD)
-        new_fitness = float(self.util.get_last_chars(got[0]))
-        self.log.info(f'evalIntoAndFromLean, new fitness {new_fitness}')
-        return new_fitness, # returns a float in a tuple, i.e. 16.6,
-
-    def eval_test_5_2(self, individual):
-        # Transform the tree expression in a callable function
-        func = self.gp.toolbox.compile(expr=individual)
-        new_fitness = 0
-        check = 'hello_world'
-        self.log.info(f'eval_test_5_2, PRINT INDIVIDUAL >>> {individual}')
-        try:
-            self.log.info(f'eval_test_5_2, RUN? >>> {func(check)}')
-            log_file_path = util_dict['CONDOR_LOG']
-            out = self.util.get_key_line_in_lim(
-                                    check, log_filepath = log_file_path)
-            if check in out[0]: new_fitness = 100
-        except BaseException as e:
-            self.log.info(f'eval_test_5_2, ERROR >>> {e}')
-            new_fitness = -10
-        self.log.info(f'eval_test_5_2, set fitness: {new_fitness}')
-        return new_fitness,
-
-    def eval_test_5(self, individual):
-        # Transform the tree expression in a callable function
-        func = self.gp.toolbox.compile(expr=individual)
-        self.log.info(f'eval_test_5, OUTPUTTING IND >>> \n {individual}')
-        new_fitness = 0
-        config_to_run = lean_dict['LEAN_INJECTED_ALGO_JSON']
-        self.util.cp_inject_algo_in_n_sort('_test_05.py','')
-        try:
-            self.lean.run_lean_via_CLI('main.py', config_to_run)
-        except BaseException as e:
-            self.log.error("eval_test_5, attempting Lean run", str(e))
-        self.log.info(f'eval_test_5, new fitness {new_fitness}')
-        return new_fitness,
-
     def eval_test_6(self, individual):
         # Transform the tree expression in a callable function
         func = self.gp.toolbox.compile(expr=individual)
@@ -166,11 +118,13 @@ class GpControl:
         except Exception as e:
             self.log.debug(f'{individual} not wrapped: {str(e)}')
             new_fitness = -100.0
-        config_to_run = lean_dict['LEAN_INJECTED_ALGO_JSON']
+        config_to_run = self.p.lean_dict['LEAN_INJECTED_ALGO_JSON']
         try:
             if self.run_backtest:
                 self.log.debug("GpControl.eval_test_6 >>>> RUN LEAN >>>>")
-                self.lean.run_lean_via_CLI('main.py', config_to_run)
+            # LEAN HANGOVER:
+            # # # # # # # #
+                self.backtester.run_lean_via_CLI('main.py', config_to_run)
                 new_fitness = self.gpf.get_fit_6()
             else:
                 self.log.debug("<< WOULD RUN LEAN HERE >>>>>>>>>>>>>>>>>>>>>>>")
@@ -178,6 +132,46 @@ class GpControl:
             self.log.error("ERROR eval_test_6, attempting Lean run")
 
         self.log.info(f'eval_test_6, new fitness {new_fitness}')
+        return new_fitness, # returns a float in a tuple, i.e.  14736.68,
+
+    def eval_nautilus(self, individual):
+        '''
+        First inclusion of Nautilus in evaluation function
+        Set as the default evaluator, see gp_control.__init__
+        Moving gradually from Lean over to Nautilus
+        '''
+        # TODO:
+        evalf_name = 'eval_nautilus'
+
+        # Transform the tree expression in a callable function
+        func = self.gp.toolbox.compile(expr=individual) # Deap reqmt, not used
+        self.log.info(f'{evalf_name}, OUTPUTTING IND >>> \n {individual}')
+        new_fitness = 0.0
+        # additional code to inject evolved code individual
+        try:
+            # LEAN HANGOVER:
+            # # # # # # # #
+            # #
+            # self.util.cp_inject_algo_in_n_sort('_test_06.py', str(individual))
+            pass
+            # TODO: something here, but kept out for now
+        except Exception as e:
+            self.log.debug(f'eval_nautilus: {individual} not wrapped: {str(e)}')
+            new_fitness = -100.0
+
+        try:
+            if self.run_backtest:
+                self.log.debug(f"GpControl.{evalf_name} >>>> RUN NAUTILUS >>>>")
+                self.backtester.basic_run_through()
+                new_fitness = self.gpf.get_fit_nautilus_1()
+                self.log.debug(f"GpControl.{evalf_name} >>>> NAUTILUS fitness {new_fitness} >>>>")
+
+            else:
+                self.log.debug(f"<< ERROR eval_nautilus >>> RUN NAUTILUS HERE >>>>>>>>>>> {new_fitness}")
+        except BaseException as e:
+            self.log.error(f"ERROR {evalf_name}, attempting Nautilus run: {e}")
+
+        self.log.info(f'GpControl.{evalf_name}, new fitness {new_fitness}')
         return new_fitness, # returns a float in a tuple, i.e.  14736.68,
 
 if __name__ == "__main__":
