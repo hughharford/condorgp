df57f116fd2319f457ac66b6d5d20a8770660c1d
Hugh Harford <hugh.harford@poscoconsulting.com>
2026-01-30 23:21:04 +0000

    failing test for microk8s start
    Signed-off-by: Hugh Harford <hugh.harford@poscoconsulting.com>

--- k8s/reg_start_k8s.sh
+++ k8s/reg_start_k8s.sh
@@ -11,6 +11,7 @@ microk8s enable hostpath-storage
 microk8s enable ingress
 microk8s enable registry
 microk8s enable dashboard
+microk8s enable rbac
 
 # microk8s kubectl describe secret -n kube-system microk8s-dashboard-token
 
